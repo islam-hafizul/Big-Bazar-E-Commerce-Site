b8461d960f9f5acedb635f627ed2cda48bff25c4
islam-hafizul <islam.h3141@gmail.com>
2026-08-03 16:14:24 +0600

    Set up new project, add components

---
+++ src/app/pages/websites/customer-cart/customer-cart.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CustomerCart } from './customer-cart';
+
+describe('CustomerCart', () => {
+  let component: CustomerCart;
+  let fixture: ComponentFixture<CustomerCart>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CustomerCart],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CustomerCart);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/websites/customer-cart/customer-cart.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-cart',
+  imports: [],
+  templateUrl: './customer-cart.html',
+  styleUrl: './customer-cart.css',
+})
+export class CustomerCart {}
