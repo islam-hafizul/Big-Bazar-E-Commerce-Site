f8462eb2154124a9abba74041d0a55870e8e3996
islam-hafizul <islam.h3141@gmail.com>
2026-08-24 20:07:19 +0600

    add Login page and  its components

--- src/app/pages/admin/login/login.ts
+++ src/app/pages/admin/login/login.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  loginObj: any = {
+    email: '',
+    password: ''
+  }
+
+  constructor(private router: Router) {}
+  
+  onLogin() {
+    if (this.loginObj.email == "admin" && this.loginObj.password == "12341234") {
+      this.router.navigateByUrl('/products');
+    } else{
+      console.log('Invalid credentials');
+    }
+  }
+}
